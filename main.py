a8c66f93d25a89223e9990773797ae22f7215f31
Graham Joonsar <70826772+GrahamJoonsar@users.noreply.github.com>
2023-10-18 16:44:41 -0400

    Adding Initial Files

---
+++ main.py
@@ -0,0 +1,24 @@
+from comms import *
+from controls import *
+
+""" Values for the Robot """
+THRUSTERS = {
+    # Horizontal Thrusters
+    "HTL": 1500,
+    "HTR": 1500,
+    "HBL": 1500,
+    "HBR": 1500,
+
+    # Vertical Thrusters
+    "VTL": 1500,
+    "VTR": 1500,
+    "VTL": 1500,
+    "VTL": 1500
+}
+
+ARM = {
+    "TWIST": 1500,
+    "TILT":  1500,
+    "CLAW":  1500
+}
+
